e089c383772c17a494017614d1c6185be5fc3a1d
Avinash Kumar <hello.avinash@gmail.com>
2017-10-13 15:00:13 -0500

    Avinash: Java thread - lock and condition

---
+++ src/main/java/thread/ProdConUsingLockAndCondition.java
@@ -0,0 +1,78 @@
+package thread;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by akumar6 on 10/13/17.
+ */
+public class ProdConUsingLockAndCondition {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ProducerConsumerCheck pc = new ProducerConsumerCheck();
+
+        Thread t1 = new Thread(() -> {
+            try {
+                pc.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                pc.consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+    }
+}
+
+class ProducerConsumerCheck {
+
+    private Lock lock = new ReentrantLock();
+    private Condition cond = lock.newCondition();
+    private volatile boolean produced = false;
+
+    public void produce() throws InterruptedException {
+        int count = 0;
+        lock.lock();
+        while (count < 10) {
+            if (produced) {
+                cond.await();
+            } else {
+                System.out.println("Avinash");
+                count++;
+                produced = true;
+                cond.signal();
+            }
+        }
+        lock.unlock();
+
+    }
+
+    public void consume() throws InterruptedException {
+        int count = 0;
+        lock.lock();
+        while (count < 10) {
+            if (produced) {
+                System.out.println("Kumar");
+                produced = false;
+                count++;
+                cond.signal();
+            } else {
+                cond.await();
+            }
+        }
+
+    }
+
+}
